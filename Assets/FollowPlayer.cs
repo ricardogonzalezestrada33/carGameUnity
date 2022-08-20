7d9b403362e527d3442985e65e4af76923d50374
Lout33 <lyupanqui99@gmail.com>
2022-08-20 14:40:11 -0500

    part8:      -add 3d assets      -delete test boxs

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-	private Vector3 offset = new Vector3(0,2,-7);
+	private Vector3 offset = new Vector3(0,5,-10);
     // Start is called before the first frame update
     void Start()
     {
